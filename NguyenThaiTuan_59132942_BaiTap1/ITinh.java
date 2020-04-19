82616196638283c659f9734f26139715f07a68b7
nguyenthaituan <12c6nguyenthaituan@gmail.com>
2020-04-19 16:17:17 +0700

    BaiTapJava_tuan2

---
+++ NguyenThaiTuan_59132942_BaiTap1/ITinh.java
@@ -0,0 +1,8 @@
+
+
+
+package NguyenThaiTuan_59132942_BaiTap1;
+
+public interface ITinh {
+    public float tinh(float a, float b);
+}
---
+++ NguyenThaiTuan_59132942_BaiTap1/Tru.java
@@ -0,0 +1,17 @@
+
+package NguyenThaiTuan_59132942_BaiTap1;
+
+/**
+ *
+ * @author Admin
+ */
+public class Tru implements ITinh{
+
+    public Tru() {
+    }
+    
+    @Override
+    public float tinh(float a, float b) {
+        return a - b;
+    }   
+}
